deb373149a7c4aedea5a784af1c2df61e5b87674
Mikkel Fennefoss <mikkel.lundgreen@bestseller.com>
2016-11-06 17:19:19 +0100

    Added js functionality

--- popup.js
+++ popup.js
@@ -1,5 +1,18 @@
 window.onload = function(){ 
+
+    window.addEventListener('click',function(e){
+        if(e.target.href!==undefined){
+            chrome.tabs.create({url:e.target.href});
+        }
+    });
+
 	document.getElementById("popup").onclick = function(){
-		document.getElementById("popup").className = "MyClass";
+		document.getElementById("popup").classList.add('myclass');
+	};
+
+	// click brand get country
+	document.querySelector(".jj").onclick = function(){
+		document.getElementById("country").className = "js-country";
+		document.querySelector('.country-links')
 	};
 };
